63038f207f8a43d0695588d7490cd6192dc6ecda
yurisoaresysob <164704566+yurisoaresysob@users.noreply.github.com>
2024-07-11 20:08:02 -0300

    Update callback.py
    Adicionado a função de capturar o called e o caller

--- callback.py
+++ callback.py
@@ -1,58 +1,95 @@
 from asterisk.manager import Manager
 from asterisk.agi import AGI
-import re
+import sys, re
 
-def send_call(origin, dest):
-    manager = Manager()
-    try:
-        manager.connect(AMI_HOST, AMI_PORT)
-        manager.login(AMI_USERNAME, AMI_PASSWORD)
-        print("Conexão bem-sucedida com o Asterisk Manager Interface!")
-    except Exception as e:
-        print("Erro ao conectar ao Asterisk Manager Interface:", e)
-        exit()
+# Funções
+def main():
+    agi.verbose("o Script AGI foi iniciado")
+
+    # Capturar os números de origem e destino
    
-    # Criar a ação Originate
-    action = {
-        'Action': 'Originate',
-        'Channel': f'PJSIP/{origin}',
-        'Exten': dest,
-        'Context': 'ramais',
-        'Priority': 1,
-        'CallerID': origin,
-        'Timeout': 10000  # Timeout em milissegundos
-    }
+    rege_number = re.compile(r'\d{4}')
 
-    # Enviar a ação
+    destination = agi.env['agi_extension']
+    channel = agi.env['agi_channel']
+
+
+    channel = rege_number.search(channel)
+    if channel == None:
+        ...
+    else:
+        channel = channel.group()
+
+    agi.verbose(f"Destination: {destination}")
+    agi.verbose(f"Channel: {channel}")
+
+    origin_dest = [channel, destination]
+
+    return origin_dest
+def check_channel(event, manager):
+    global state_channel
+
+    # Expressão regular para capturar o número do Device
     try:
-        response = manager.send_action(action)
-        print("Resposta do Asterisk:", response)
-    except Exception as e:
-        print("Erro ao enviar a ação para o Asterisk:", e)
+        regex = re.compile(r'\d{4}')
+    
+    finally:    
 
-    # Desconectar do AMI
+        # Subalgoritmo
+        if event.name == 'DeviceStateChange':
+            number = event.get_header('Device')
+            number = regex.search(number)
+            if number == None:
+                ...
+            else:
+                number = number.group()
+
+            state = event.get_header('State')
+
+            if number == called:
+                print(f'{number}  -->  {state}')
+                
+                if state == 'INUSE':
+                    print('Este ramal está ocupado')
+                    state_channel = 'y'
+                    sys.exit()
+
+global caller
+global called
+
+manager = Manager()
+agi = AGI()
+state_channel = ''
+
+# Capturar o número chamador e o chamado
+list_origin_dest = main()
+caller = list_origin_dest[0]
+called = list_origin_dest[1] 
+
+# Verifica se o canal está em uso
+try:
+    # Login na central via AMI
+    manager.connect('10.40.165.200', 5038)
+    manager.login('ttag', 'panopreto')
+    
+    print('Monitorando o canal...\nPressione Ctrl + c para sair...')
+    # Verifica o Status do canal
+
+    action = {
+        'Action': 'DeviceStateList'
+    }
+    manager.register_event('DeviceStateChange', check_channel)
+    manager.send_action(action)
+
+    try:
+        while True:
+            manager.event_dispatch()
+          
+    except SystemExit:
+        print(f'O ramal {called} esta´ocupado. Deseja efetuar o callback?\nDigite 1 para Sim ou 2 para não.')
+
+    # Logoff da Central
     manager.logoff()
     manager.close()
-
-
-# Parâmetros de conexão com o Asterisk Manager Interface (AMI)
-AMI_HOST = '10.40.165.200'  # Endereço IP do servidor Asterisk
-AMI_PORT = 5038  # Porta do Asterisk Manager Interface
-AMI_USERNAME = 'ttag'  # Nome de usuário do AMI
-AMI_PASSWORD = 'panopreto'  # Senha do AMI
-
-# Conectando ao Asterisk Manager Interface (AMI)
-
-agi = AGI()
-rege_number = re.compile(r'\d{4}')
-
-destino = agi.env['agi_extension'].strip()
-origem = agi.env['agi_channel']
-origem = rege_number.search(origem).group()
-origem = origem.strip()
-
-agi.verbose(f'Ligação de {origem} para {destino}')
-
-send_call(origem, destino)
-
-agi.verbose('Efetuando ligação entre os ramais...')
+except Exception:
+   print('Programa finalizado')
